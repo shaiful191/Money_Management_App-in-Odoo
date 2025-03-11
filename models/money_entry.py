9a651ad8a7a20455cb1ed45642f1aa440b28f0cf
Shaiful191 <saifulislamridoy1999@gmail.com>
2025-03-11 12:07:15 +0600

    compute balance field create and server action notification to show current balance

--- models/money_entry.py
+++ models/money_entry.py
@@ -16,3 +16,33 @@ class MoneyEntry(models.Model):
           string='Type',
           required=True
   )
+
+  balance = fields.Float(compute = '_compute_balance', string='Balance',readonly=True)
+
+  @api.depends('type','amount')
+  def _compute_balance(self): 
+      
+      all_records = self.env['money.entry'].search([])
+
+      total_income = sum(all_records.filtered(lambda r: r.type == 'income').mapped('amount'))
+      total_expenses = sum(all_records.filtered(lambda r: r.type == 'expense').mapped('amount'))
+ 
+      for record in self:
+          record.balance = total_income - total_expenses
+
+  def get_balance2(self):
+     
+     all_records = self.env['money.entry'].search([])
+
+     total_income = sum(all_records.filtered(lambda r: r.type == 'income').mapped('amount'))
+     total_expenses = sum(all_records.filtered(lambda r: r.type == 'expense').mapped('amount'))
+     balance = total_income - total_expenses
+     return {
+        'type': 'ir.actions.client',
+        'tag':'display_notification',
+        'params': {
+            'title': 'Current Balance',
+            'message': f'Your current balance is {balance}',
+            'sticky': False
+        }
+     } 
